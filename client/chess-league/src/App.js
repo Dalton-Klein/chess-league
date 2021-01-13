969e5fe18fa288cadc7110aa2cb70c0fafb9853c
Dalton Klein <deklein@live.com>
2021-01-13 09:13:46 -0600

    Got HTML5 Canvas Working In React

---
+++ client/chess-league/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Canvas from './components/canvas'
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas></Canvas>
+    </div>
+    
+  )
+}
+
